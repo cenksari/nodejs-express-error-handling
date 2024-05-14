c86f71dec2101fbf7a7da0ad92d9b19f0523cc40
Cenk SARI <cenk@cenksari.com>
2024-05-14 16:28:31 +0100

    bug fix

--- src/errors/CustomError.ts
+++ src/errors/CustomError.ts
@@ -2,6 +2,8 @@ export abstract class CustomError extends Error {
   constructor(public message: string) {
     super(message);
   }
+
   abstract StatusCode: number;
+
   abstract serialize(): { message: string };
 }
--- src/middlewares/errorHandlerMiddleware.ts
+++ src/middlewares/errorHandlerMiddleware.ts
@@ -1,15 +1,18 @@
-import { ErrorRequestHandler, Request, Response } from 'express';
+import { ErrorRequestHandler, Request, Response, NextFunction } from 'express';
 
 import { CustomError } from '../errors/CustomError';
 
 export const errorHandlerMiddleware: ErrorRequestHandler = (
   error: Error,
   req: Request,
-  res: Response
+  res: Response,
+  next: NextFunction
 ) => {
   if (error instanceof CustomError) {
-    return res.status(error.StatusCode).send(error.serialize());
+    res.status(error.StatusCode).send(error.serialize());
   }
 
-  return res.status(500).send({ message: `Something bad happened: ${error.message}` });
+  res.status(500).send({ message: `Something bad happened: ${error.message}` });
+
+  next();
 };
